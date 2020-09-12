f65eaf0f6ca2047e91651326df60e1ab8cde79e0
o0pmitev <pmitev89@gmail.com>
2020-09-12 12:44:24 +0200

    Add Flat routes and MongoDb connect

--- server.js
+++ server.js
@@ -4,34 +4,38 @@ const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const hpp = require('hpp');
 
-// const connectDB = require('./config/db');
+// Connect to MongoDb
+const connectDB = require('./config/db');
+connectDB();
 
 dotenv.config({ path: './config/config.env' });
 
-// connectDB();
 
 const app = express();
-
-app.use(express.json());
 
 const rateLimiter = rateLimit({
   windowMs: 10 * 60 * 1000,
   max: 10,
 });
 
+// Init Middleware
+app.use(express.json({ extended: false }));
 app.use(cors());
-
 app.use(rateLimiter);
 app.use(hpp());
 
+
+
+// Define Routes
 app.get("/contact", (req,res)=>{
   res.send("5000 running")
 })
-
-
 app.use('/contact', require('./routes/contactRoutes'));
+app.use('/api/flats', require('./routes/api/flats'));
 
-const server = app.listen(process.env.PORT || '5000', () => console.log('hello'));
+// Start the server
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT || '5000', () => console.log(`Server is running on port ${ PORT }`));
 
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Error ${err.message}`);
